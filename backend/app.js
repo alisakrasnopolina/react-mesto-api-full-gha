40d18b28056e3c182bdc16d8a903bdc29e0e4f5e
Alisa Krasnopolina <alisakras9@gmail.com>
2023-05-18 18:41:07 +0300

    connect backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 const corsOptions = {
-  origin: '*',
+  origin: true,
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   allowedHeaders: ['Content-type', 'Authorization'],
   credentials: true,

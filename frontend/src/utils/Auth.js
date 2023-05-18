022b8b6584449b0f0e432acb5d4e60d732c29e8f
Alisa Krasnopolina <alisakras9@gmail.com>
2023-05-18 19:04:13 +0300

    connect backend and frontend

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -35,6 +35,7 @@ export const authorization = (email, password) => {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
+    credentials: 'include',
     body: JSON.stringify({
       email: email,
       password: password
